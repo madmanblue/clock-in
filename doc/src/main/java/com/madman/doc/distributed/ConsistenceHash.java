80cb632762caa2da690f1d56cd9fd8d7d4bacce8
blue <1327128544@qq.com>
2020-01-08 18:11:24 +0800

    consistence hash

---
+++ doc/src/main/java/com/madman/doc/distributed/ConsistenceHash.java
@@ -0,0 +1,12 @@
+package com.madman.doc.distributed;
+
+/**
+ * 一致性hash
+ * https://cloud.tencent.com/developer/news/265055
+ * https://www.cnblogs.com/study-everyday/p/8629100.html
+ *
+ * 创建虚拟节点映射到实际节点上防止数据倾斜，确保很少的节点也能均匀的将数据分配
+ *
+ */
+public class ConsistenceHash {
+}
