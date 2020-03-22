2d8a3ae6a85e4c3be9288eb4696ce838723dae50
blue <1327128544@qq.com>
2020-03-22 16:42:16 +0800

    md

--- doc/src/main/java/com/madman/doc/evn/centos搭建java环境.md
+++ doc/src/main/java/com/madman/doc/evn/centos搭建java环境.md
@@ -1,13 +1,13 @@
 # CentOS中Java环境搭建
 
- __yum安装__  
+ ___yum安装___  
    1. 首先删除原有Java , 查看命令（java -version 或 rpm -qa ｜ grep java）  
    2. 使用命令 yum list java*查看Java包并下载对应版本, yum -y install java-1.8.0-openjdk  
    3. java -version查看是否安装成功  
    4. 配置Java环境变量 whereis java ， cd /usr /lib/jvm/   java-1.8.....x86_64就是JAVA_HOME  
    5. vim /etc/profile  
-      export JAVA_HOME=/usr/lib/jvm/java-...../jre
-      export JRE_HOME=${JAVA_HOME}
-      export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
-      export PATH=${JAVA_HOME}/bin:$PATH
+      export JAVA_HOME=/usr/lib/jvm/java-...../jre  
+      export JRE_HOME=${JAVA_HOME}  
+      export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib  
+      export PATH=${JAVA_HOME}/bin:$PATH  
    6. source /etc/profile
--- future/src/test/java/com/madman/future/FutureApplicationTests.java
+++ future/src/test/java/com/madman/future/FutureApplicationTests.java
@@ -1,12 +1,72 @@
 package com.madman.future;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootTest
 class FutureApplicationTests {
 
-    @Test
-    void contextLoads() {}
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @Test
+    void contextLoads() {
+        String sql = "insert into test (name) values ('张三')";
+        jdbcTemplate.update(sql);
+    }
+
+    @Test
+    void testGet() {
+        String sql = "select * from test";
+
+        List<User> query = jdbcTemplate.query(sql, new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                int id = resultSet.getInt("id");
+                User user = new User();
+                user.setId(id);
+                String name = resultSet.getString("name");
+                user.setName(name);
+
+                return user;
+            }
+        });
+
+        User user = query.get(0);
+        System.out.println(user);
+
+    }
+}
+
+class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
 }
