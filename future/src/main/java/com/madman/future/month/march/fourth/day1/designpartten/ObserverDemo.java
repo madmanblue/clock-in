348fa2f387b148d7322deb13cdc18dadf6424868
madman <1327128544@qq.com>
2020-04-20 22:42:49 +0800

    'ignore'

--- future/src/main/java/com/madman/future/month/march/fourth/day1/designpartten/ObserverDemo.java
+++ future/src/main/java/com/madman/future/month/march/fourth/day1/designpartten/ObserverDemo.java
@@ -1,4 +1,50 @@
 package com.madman.future.month.march.fourth.day1.designpartten;
 
-public class ObserverDemo {
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ObserverDemo {
+
+    static List<Observer> observers = new ArrayList<>();
+    {
+        observers.add(new A());
+        observers.add(new B());
+    }
+
+    void  pubJob(){
+        for (Observer observer : observers) {
+            observer.some();
+        }
+    }
+    public static void main(String[] args) {
+        ObserverDemo demo = new ObserverDemo();
+        demo.pubJob();
+
+    }
+
+
+
+
+    interface Observer{
+        void some();
+    }
+
+    class A implements Observer{
+
+        @Override
+        public void some() {
+            System.out.println("A");
+        }
+    }
+
+    class B implements Observer{
+
+        @Override
+        public void some() {
+            System.out.println("B");
+        }
+    }
+
 }
+
