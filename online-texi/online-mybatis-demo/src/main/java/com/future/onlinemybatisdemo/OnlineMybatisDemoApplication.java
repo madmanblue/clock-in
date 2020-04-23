540675db5ba68e5d839a7db4f38b6c209f463584
madman <1327128544@qq.com>
2020-04-24 01:59:46 +0800

    [init]online-texi

---
+++ online-texi/online-mybatis-demo/src/main/java/com/future/onlinemybatisdemo/OnlineMybatisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.future.onlinemybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMybatisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMybatisDemoApplication.class, args);
+    }
+
+}
---
+++ online-texi/online-mybatis-demo/src/test/java/com/future/onlinemybatisdemo/OnlineMybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.onlinemybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMybatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
